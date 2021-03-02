8b8ced9c3126a9e420e0ee35a33fa6a9a469e777
github username <github email address>
2021-03-02 15:13:21 -0600

    commit tree 0b4d843d14ba31ae59827f3ca4ad106cd6ea0a08

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+function titleCaseTutorials(tutorial) {
+  let words = tutorial.split(" ") 
+  // splits each letter of the strings
+  let wordsCap = words.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+  // iteratiing with .map
+  // word is paramater, grabbing the character at 0, making it upper case, then adding the rest of the sentence.
+  let tutorialCap = wordsCap.join(" ")
+  // joining all the spilt string again.
+  return tutorialCap
+}
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => titleCaseTutorials(tutorial))
 }
